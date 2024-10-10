49dfcf4d7d2c49c592c56f677c192c264e7b3b1f
Juan Pablo Villaplana Corrales <jpvillaplana@gmail.com>
2024-10-09 23:58:13 -0600

    comment tickets models

--- models/ticketModel.ts
+++ models/ticketModel.ts
@@ -4,46 +4,50 @@ const prisma = new PrismaClient();
 
 class TicketModel{
 
-    static async createWithEvent(body: any){
-        const { eventId, ownerId, type, price } = body;
+    // static async createWithEvent(body: any){
+    //     const { eventId, ownerId, type, price } = body;
 
-        const {} = body;
+    //     const {} = body;
 
-        const ticket = await prisma.ticket.create({
-            data: {
-                eventId,
-                ownerId,
-                type,
-                isUsed: false,
-                price
-            }
-        })
+    //     const ticket = await prisma.ticket.create({
+    //         data: {
+    //             eventId,
+    //             ownerId,
+    //             type,
+    //             isUsed: false,
+    //             price,
+    //             image,
+    //             builderScore
+    //         }
+    //     })
 
-        return ticket;
-    }
+    //     return ticket;
+    // }
 
-    static async updateTicket(ticketId: number, body: any){
-        const { type, price, isUsed } = body;
+    // static async updateTicket(ticketId: number, body: any){
+    //     const { type, price, isUsed } = body;
 
-        const ticket = await prisma.ticket.update({
-            where: { id: ticketId },
-            data: {
-                type,
-                price,
-                isUsed
-            }
-        });
+    //     const ticket = await prisma.ticket.update({
+    //         where: { id: ticketId },
+    //         data: {
+    //             type,
+    //             price,
+    //             isUsed,
+    //             image,
+    //             builderScore
+    //         }
+    //     });
 
-        return ticket;
-    }
+    //     return ticket;
+    // }
 
-    static async getTicketById(ticketId: number){
-        const ticket = await prisma.ticket.findUnique({ where: { id: ticketId } });
+    // static async getTicketById(ticketId: number){
+    //     const ticket = await prisma.ticket.findUnique({ where: { id: ticketId } });
 
-        if(!ticket) throw new Error('Ticket not found');
+    //     if(!ticket) throw new Error('Ticket not found');
 
-        return ticket;
-    }
+    //     return ticket;
+    // }
 
     
 }
